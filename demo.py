d4405e10895b431817c71a20bcc66e4db2f62ee6
rushikeshnayakavadi <rushikeshnayakavadi@gmail.com>
2025-02-21 11:24:16 +0530

    ready till model pusher

--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
-# below code is to check the logging config
-from src.logger import logging
+# # below code is to check the logging config
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
